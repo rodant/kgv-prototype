b8d6da80ba643be763d6529346c38b4201b97712
Antonio Rodriguez <antonio.santiesteban@leanovate.de>
2019-02-03 17:10:57 +0100

    KGV-11: facade for the rdflib.

---
+++ src/main/scala/me/spoter/rdflib/RDFLib.scala
@@ -0,0 +1,23 @@
+package me.spoter.rdflib
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.{JSGlobal, JSImport, JSName}
+
+/**
+  *
+  */
+@js.native
+@JSImport("rdflib", JSImport.Default)
+object RDFLib extends js.Object {
+  def Namespace(uri: String): js.Dynamic = js.native
+
+  def graph(): js.Dynamic = js.native
+
+  def sym(subject: String): js.Dynamic = js.native
+}
+
+@js.native
+@JSImport("rdflib", "Fetcher")
+class RDFFetcher(store: js.Dynamic) extends js.Object {
+  def load(subject: String): js.Promise[js.Object] = js.native
+}
