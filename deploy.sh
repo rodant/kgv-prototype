98ff4aace663bc84a4c9b9f0d68001395cef0db3
Antonio Rodriguez <antonio.santiesteban@leanovate.de>
2019-02-11 12:10:57 +0100

    KGV-11: small improvement of deployment script.

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,14 @@ cp target/scala-2.12/scalajs-bundler/main/kgv-prototype-opt-bundle.js public/kgv
 
 echo "Uploading the App to the Solid Server..."
 curl --cookie "connect.sid=$SOLID_COOKIE" --upload-file index.html https://spoterme.solid.community/index.html
+echo ":-)"
 curl --cookie "connect.sid=$SOLID_COOKIE" --upload-file public/kgv/css/app.css https://spoterme.solid.community/public/kgv/css/app.css
+echo ":-)"
 curl --cookie "connect.sid=$SOLID_COOKIE" --upload-file public/kgv/js/kgv-prototype-opt-bundle.js https://spoterme.solid.community/public/kgv/js/kgv-prototype-opt-bundle.js
+echo ":-)"
 curl --cookie "connect.sid=$SOLID_COOKIE" --upload-file public/kgv/images/image-1.svg https://spoterme.solid.community/public/kgv/images/image-1.svg
+echo ":-)"
 curl --cookie "connect.sid=$SOLID_COOKIE" --upload-file public/kgv/images/image-2.svg https://spoterme.solid.community/public/kgv/images/image-2.svg
+echo ":-)"
 curl --cookie "connect.sid=$SOLID_COOKIE" --upload-file public/kgv/images/image-3.svg https://spoterme.solid.community/public/kgv/images/image-3.svg
+echo "Done :-)"
