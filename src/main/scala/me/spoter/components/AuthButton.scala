dd1548918a18ccf684895065761af0a0a188e70c
Antonio Rodriguez <antonio.santiesteban@leanovate.de>
2019-02-22 12:11:10 +0100

    KGV-11: track session in TopNav and propagate to the AuthButton through props.

--- src/main/scala/me/spoter/components/AuthButton.scala
+++ src/main/scala/me/spoter/components/AuthButton.scala
@@ -1,6 +1,5 @@
 package me.spoter.components
 
-import japgolly.scalajs.react.component.builder.Lifecycle.ComponentDidMount
 import japgolly.scalajs.react.vdom.html_<^._
 import japgolly.scalajs.react.{BackendScope, Callback, ScalaComponent}
 import me.spoter.components.bootstrap.Button
@@ -13,18 +12,16 @@ import scala.scalajs.js
   */
 object AuthButton {
 
-  case class Props(popupUri: String, callbackUri: String)
+  case class Props(popupUri: String, callbackUri: String, loggedIn: Boolean)
 
   private val component = ScalaComponent
     .builder[Props]("AuthButton")
-    .initialState(false)
     .renderBackend[Backend]
-    .componentDidMount(c => c.backend.trackSession(c))
     .build
 
-  class Backend(bs: BackendScope[Props, Boolean]) {
-    def render(loggedIn: Boolean): VdomElement = {
-      if (!loggedIn)
+  class Backend(bs: BackendScope[Props, Unit]) {
+    def render(props: Props): VdomElement = {
+      if (!props.loggedIn)
         Button()(^.onClick --> login)("Einlogen")
       else
         Button()(^.onClick --> logout)("Ausloggen")
@@ -40,16 +37,8 @@ object AuthButton {
       SolidAuth.logout()
       ()
     }
-
-    def trackSession(c: ComponentDidMount[Props, Boolean, Backend]): Callback = Callback {
-      SolidAuth.trackSession { s =>
-        (if (s != null)
-          c.modState(_ => true)
-        else
-          c.modState(_ => false)).runNow()
-      }
-    }
   }
 
-  def apply(popupUri: String, callbackUri: String = null): VdomElement = component(Props(popupUri, callbackUri)).vdomElement
+  def apply(popupUri: String, callbackUri: String = null, loggedIn: Boolean = false): VdomElement =
+    component(Props(popupUri, callbackUri, loggedIn)).vdomElement
 }
