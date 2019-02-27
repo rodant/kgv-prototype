60489d4309d775fddf1bca4597abe59e43c5ab35
Antonio Rodriguez <antonio.santiesteban@leanovate.de>
2019-02-27 16:23:48 +0100

    KGV-23 better support for mobile.

--- src/main/scala/me/spoter/components/TopNav.scala
+++ src/main/scala/me/spoter/components/TopNav.scala
@@ -32,9 +32,8 @@ object TopNav extends SessionTracker[Unit, Unit, Unit] {
         Nav()(^.className := "mr-auto")(
           NavLink(href = "#gardens")("Meine Gärten"),
           NavLink(href = "#offerings")("Meine Gartenangebote")),
-        Form(inline = true)(^.id := "login-button", ^.className := "ui-elem",
-          <.div(^.id := "logged-in-user", ^.className := "ui-elem", Value("user.name")).when(loggedIn),
-          <.div(^.id := "login-button", ^.className := "ui-elem",
-            AuthButton("https://solid.community/common/popup.html", loggedIn = loggedIn)))))
+        Nav()(^.id := "login-button",
+          NavBarText()(^.id := "logged-in-user", ^.className := "ui-elem", Value("user.name")).when(loggedIn),
+          <.div(^.id := "login-button", AuthButton("https://solid.community/common/popup.html", loggedIn = loggedIn)))))
   }
 }
