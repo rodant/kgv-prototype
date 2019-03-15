ed378888d940034875783d66694086b521cb28e8
Antonio Rodriguez <antonio.santiesteban@leanovate.de>
2019-03-15 19:38:19 +0100

    KGV-25 initial entities with default lang "de".

--- src/main/scala/me/spoter/models/AllotmentGarden.scala
+++ src/main/scala/me/spoter/models/AllotmentGarden.scala
@@ -5,26 +5,28 @@ import java.net.URI
 import me.spoter.models.AllotmentCondition.Good
 import me.spoter.services.GardenService.RdfLiteral
 
+import scala.scalajs.js.UndefOr
+
 /**
   * AllotmentGarden entity.
   *
   */
-case class AllotmentGarden private(uri: URI = URI.create(""),
-                                   title: RdfLiteral = RdfLiteral(""),
-                                   address: Address = Address("", 0, "", ""),
-                                   location: Location = Location(0, 0),
-                                   images: Seq[URI] = List(URI.create("public/kgv/images/image-1.svg")),
-                                   description: String = "",
-                                   area: Area = Area(),
-                                   bungalow: Option[Bungalow] = None,
-                                   condition: AllotmentCondition = Good) extends KGVEntity {
+case class AllotmentGarden private(uri: URI,
+                                   title: RdfLiteral,
+                                   address: Address,
+                                   location: Location,
+                                   images: Seq[URI],
+                                   description: String,
+                                   area: Area,
+                                   bungalow: Option[Bungalow],
+                                   condition: AllotmentCondition) extends KGVEntity {
 
   override def withNewTitle(t: RdfLiteral): KGVEntity = copy(title = t)
 }
 
 object AllotmentGarden {
   def apply(uri: URI = URI.create(""),
-            title: RdfLiteral = RdfLiteral(""),
+            title: RdfLiteral = RdfLiteral("", UndefOr.any2undefOrA("de")),
             address: Address = Address("", 0, "", ""),
             location: Location = Location(0, 0),
             images: Seq[URI] = List(URI.create("public/kgv/images/image-1.svg")),
--- src/main/scala/me/spoter/models/AllotmentOffering.scala
+++ src/main/scala/me/spoter/models/AllotmentOffering.scala
@@ -5,12 +5,13 @@ import java.net.URI
 import me.spoter.services.GardenService.RdfLiteral
 
 import scala.scalajs.js
+import scala.scalajs.js.UndefOr
 
 /**
   * A garden offering.
   */
 case class AllotmentOffering(uri: URI = URI.create(""),
-                             title: RdfLiteral = RdfLiteral(""),
+                             title: RdfLiteral = RdfLiteral("", UndefOr.any2undefOrA("de")),
                              description: String = "",
                              price: Money = Money(0),
                              availabilityStarts: js.Date = new js.Date(),
